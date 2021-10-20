d6633948f614844dbc804a01facea9bdba3119a6
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2021-10-19 22:06:16 -0500

    init, RCs and aliases

---
+++ .config/aliases.sh
@@ -0,0 +1,22 @@
+alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# meta: alias file
+alias al="open ~/.oh-my-zsh/custom/aliases.zsh"
+
+# zshrc file
+alias rc="open ~/.zshrc"
+
+# open in sublime text
+alias sublime="subl"
+
+# --- R ---
+
+# from radian repo (could be outdated...)
+alias radian="~/Library/Python/3.7/bin/radian"
+alias r="radian"
+# open profiles
+alias rp="open ~/.rprofile; open ~/.radian_profile"
+# prevent R from doing any workspace nonsense
+alias R="R --no-save --no-restore"
+
+
