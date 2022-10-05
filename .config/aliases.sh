6cd4149440178ff474fef396a40be3c6179ec155
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2022-10-04 21:40:50 -0500

    trim aliases, fewer `open` cmds

--- .config/aliases.sh
+++ .config/aliases.sh
@@ -1,15 +1,14 @@
 alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dots-r='/usr/bin/git --git-dir=$HOME/.dotfiles_R/ --work-tree=$HOME'
-alias dots-vim='/usr/bin/git --git-dir=$HOME/.dotfiles_vim/ --work-tree=$HOME'
 
-# meta: alias file
-alias al="open $HOME/.oh-my-zsh/custom/aliases.zsh"
+# --- cmd line tools ---
 
-# zshrc file
-alias rc="open ~/.zshrc"
+alias cat="bat"
+alias ls="exa"
+alias ll="exa -alh"
+alias tree="exa --tree"
+alias lg="lazygit"
 
-# open in sublime text
-alias sublime="subl"
 
 # --- R ---
 
@@ -17,8 +16,7 @@ alias sublime="subl"
 # alias radian="~/Library/Python/3.7/bin/radian"
 # alias radian="/opt/homebrew/Caskroom/miniconda/base/bin/radian"
 alias r="radian"
-# open profiles
-alias rp="open $HOME/.rprofile; open $HOME/.radian_profile"
+
 # prevent R from doing any workspace nonsense
 # alias R="R --no-save --no-restore"
 
