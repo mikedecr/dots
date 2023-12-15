72589c10484d2baec6a8e80a3bea47e8ae082569
mikedecr <mgdecrescenzo@gmail.com>
2023-12-14 22:16:41 -0600

    cleanup aliases

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -3,22 +3,20 @@ alias dots-r='/usr/bin/git --git-dir=$HOME/.dotfiles_R/ --work-tree=$HOME'
 
 # --- cmd line tools ---
 
-alias ll="exa -l"
-alias la='exa -la'
+# lazygit
 alias lg="lazygit"
 alias lg-dots='lazygit -g $HOME/.dotfiles -w $HOME'
 
-alias cat="bat"
-alias ls="exa"
-alias ll="exa -alh"
+# exa
+alias ll="exa -l"
+alias la='exa -la'
+# alias ls="exa"
+alias lh="exa -alh"
 alias tree="exa --tree"
 
+# alias cat="bat"
 
 # --- R ---
-
-# from radian repo (could be outdated...)
-# alias radian="~/Library/Python/3.7/bin/radian"
-# alias radian="/opt/homebrew/Caskroom/miniconda/base/bin/radian"
 alias r="radian"
 
 # prevent R from doing any workspace nonsense
